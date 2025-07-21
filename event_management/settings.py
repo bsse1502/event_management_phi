2dec010bc8fdfe7818053795a94df6500631571b
MD. Merajul Islam <bsse1502@iit.du.ac.bd>
2025-07-21 17:21:33 +0600

    database setup done

--- event_management/settings.py
+++ event_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,15 +88,23 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 
 
 # database for postgress
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_management',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_management',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_manager_r7xg_user:qtIr4qtutMgZOdVoEeRp5qC7mFkaOrXB@dpg-d1v1ujidbo4c73f3bfk0-a.oregon-postgres.render.com/event_manager_r7xg',
+        conn_max_age=600
+    )
 }
 
 # Password validation
